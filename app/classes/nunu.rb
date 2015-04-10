7fb1234159aeb3ba52eff24fa7d90ea117bd4e83
Joseph Yi <dissonance@gmail.com>
2015-04-09 21:53:45 -0700

    fixes for workers

--- app/classes/nunu.rb
+++ app/classes/nunu.rb
@@ -1,10 +1,13 @@
 class Nunu
-  def self.consume(region:, bucket_time:, callback: callback)
+  def self.consume(region:, bucket_time:)
     client = Taric.client(region: region)
     match_ids = client.api_challenge_match_ids(begin_date: bucket_time)
 
     result = UrfIdsRequest.where(region: region, bucket_time: bucket_time).first
     UrfIdsRequest.create!(region: region, response: match_ids, bucket_time: bucket_time) if result.nil?
     match_ids
+  rescue Taric::FaradayMiddleware::NotFound => e
+    puts "No record was found for #{region} at #{bucket_time} - #{e}"
+    []
   end
 end
--- app/workers/zilean.rb
+++ app/workers/zilean.rb
@@ -5,7 +5,7 @@ class Zilean
   def perform(bucket_time)
     StaticData::REGIONS.each do |region|
       -> match_ids {
-        Chogath.feast(region: region, bucket_time: bucket_time, match_ids: match_ids)
+        Chogath.feast(region: region, bucket_time: bucket_time, match_ids: match_ids) unless match_ids.empty?
       }.(Nunu.consume(region: region, bucket_time: bucket_time))
     end
   end
